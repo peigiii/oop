c91f1c1c0fd8c425798b63cee96edaaddbd85c64
peigiii <bcs24090055@student.uts.edu.my>
2025-05-12 03:00:49 +0800

    Delete clear_history.php

--- clear_history.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-require 'db.php';
-if (!isset($_SESSION['user_id'])) exit('Not logged in');
-$user_id = $_SESSION['user_id'];
-$stmt = $pdo->prepare("DELETE FROM watch_history WHERE user_id = ?");
-$stmt->execute([$user_id]);
-echo 'ok';
