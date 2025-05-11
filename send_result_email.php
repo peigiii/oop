7fac7ef91a7863c67b563217c0f57ceb5acdbcd5
peigiii <bcs24090055@student.uts.edu.my>
2025-05-12 03:08:10 +0800

    Delete send_result_email.php

--- send_result_email.php
+++
@@ -1,8 +0,0 @@
-<?php
-function sendResultEmail($email, $score, $total) {
-    $subject = "Your SkillSphere Quiz Result";
-    $message = "Dear user,\n\nThank you for completing the quiz.\nYour score: $score / $total\n\nYou can view more details in your SkillSphere account.\n\nRegards,\nSkillSphere Team";
-    $headers = "From: no-reply@skillsphere.com\r\nContent-Type: text/plain; charset=UTF-8";
-    mail($email, $subject, $message, $headers);
-}
-?>
