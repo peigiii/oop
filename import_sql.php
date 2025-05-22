7f49bb74743778f6c4cc1efecd146ef74580aaff
peigiii <bcs24090055@student.uts.edu.my>
2025-05-23 00:57:04 +0800

    Update import_sql.php

--- import_sql.php
+++ import_sql.php
@@ -2,7 +2,7 @@
 $host = "skillspheredb.mysql.database.azure.com";
 $dbname = "skillsphere";
 $username = "peiyin5917@skillspheredb";
-$password = "0917peiyin.";  // ← 请替换成你自己的密码
+$password = "0917Peiyin.";  // ← 请替换成你自己的密码
 
 // 创建连接
 $conn = new mysqli($host, $username, $password, $dbname, 3306);
