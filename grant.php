354011d725f909026bd5e365ad69fa64f4db8d53
peigiii <bcs24090055@student.uts.edu.my>
2025-05-23 02:03:13 +0800

    Update grant.php

--- grant.php
+++ grant.php
@@ -1,13 +1,13 @@
 <?php
 $host = "skillspheredb.mysql.database.azure.com";
 $username = "peiyin5917@skillspheredb";
-$password = "0917Peiyin.";
+$password = "0917CPYcpy";
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=mysql;charset=utf8mb4", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $sql = "GRANT ALL PRIVILEGES ON skillsphere.* TO 'peiyin5917@skillspheredb'@'%' IDENTIFIED BY '0917Peiyin.'; FLUSH PRIVILEGES;";
+    $sql = "GRANT ALL PRIVILEGES ON skillsphere.* TO 'peiyin5917@skillspheredb'@'%' IDENTIFIED BY '0917CPYcpy'; FLUSH PRIVILEGES;";
     $pdo->exec($sql);
 
     echo "授权成功！";
