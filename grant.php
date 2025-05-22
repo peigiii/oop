95b2d33ce621e8d4df1b370846d085848fe71bf8
peigiii <bcs24090055@student.uts.edu.my>
2025-05-23 01:51:41 +0800

    Create grant.php

---
+++ grant.php
@@ -0,0 +1,17 @@
+<?php
+$host = "skillspheredb.mysql.database.azure.com";
+$username = "peiyin5917@skillspheredb";
+$password = "0917Peiyin.";
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=mysql;charset=utf8mb4", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $sql = "GRANT ALL PRIVILEGES ON skillsphere.* TO 'peiyin5917@skillspheredb'@'%' IDENTIFIED BY '0917Peiyin.'; FLUSH PRIVILEGES;";
+    $pdo->exec($sql);
+
+    echo "授权成功！";
+} catch (PDOException $e) {
+    echo "授权失败: " . $e->getMessage();
+}
+?>
