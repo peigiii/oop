46316412c447974e45e2a6c4601794a060797ab5
peigiii <bcs24090055@student.uts.edu.my>
2025-05-23 16:50:50 +0800

    Update db.php

--- templates/db.php
+++ templates/db.php
@@ -2,7 +2,7 @@
 // db.php
 $host = 'skillspheredb.mysql.database.azure.com';  // Azure MySQL 主机名
 $dbname = 'skillspheredb';                        // Azure 数据库名
-$user = '$user = 'peiyin5917@gmail.com';               // Azure 数据库用户名
+$user = '$user = 'peiyin5917';               // Azure 数据库用户名
 $pass = 'P12345678.';                                // Azure 密码
 
 try {
